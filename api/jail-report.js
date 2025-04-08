ef0479a65b8d09b92bf0830658d052af4746ac69
FlanCoin <flancointoken@gmail.com>
2025-04-09 01:45:29 +0200

    Descripción de los cambios

--- api/jail-report.js
+++ api/jail-report.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
   }
 
   const { data, error } = await supabase
-  .from('jail_test')
+  .from('jails')
   .insert([
     {
       uuid: jail.uuid,
